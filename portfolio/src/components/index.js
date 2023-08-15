3017d22d5ed86a4d7f102bae6fe813a21eaf1602
justinbather <justinbather@hotmail.com>
2023-08-15 19:15:39 -0400

    ide restarted to fix import error in index.js wron file name

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,9 +1,9 @@
 import Header from "./Header";
-import Test from "./Test";
+
 import Sidebar from "./Sidebar";
 import AboutMe from "./AboutMe";
 import CurrentWork from "./CurrentWork";
 import PastProjects from "./PastProjects";
 import SocialLinks from "./SocialLinks";
 
-export { Test, Header, Sidebar, AboutMe, CurrentWork, PastProjects, SocialLinks };
+export { Header, Sidebar, AboutMe, CurrentWork, PastProjects, SocialLinks };
